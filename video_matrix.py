d90eac8455dcfd192d000da308d3e73953631693
CCC <32979547+CCCpeggy@users.noreply.github.com>
2023-07-30 08:30:51 +0000

    add framework

---
+++ video_matrix.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+import random
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+class MatrixBoundaryManager:
+    ZERO = 0
+    REPEAT = 1
+    REFLECT = 2
+    def __init__(self, boundary_type="zero")
+        pass
+
+class MatrixManager:
+    def __init__(self, width, height, padding, boundary_type='reflect'):
+        pass
+        
+class TmpFileNamer:
+    def __init__(self)
+        pass
+
+class MatrixDrawer:
+    def __init__(self)
+        pass
+
+class ImageResaver:
+    def __init__(self)
+        pass
+
+class MatrixVideoGenerator:
+    def __init__(self)
+        pass
